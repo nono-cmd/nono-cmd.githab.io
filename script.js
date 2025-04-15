a96274504b4bf3628410fa66da5efc2384c00425
nono-cmd <onigiri.38flute@gmail.com>
2025-04-15 14:10:33 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeMessage() {
+  const messages = [
+    "今日も頑張ろう！",
+    "プログラミングは楽しいね！",
+    "GitHub Pages、最高！",
+    "HTMLとCSSをマスターしよう！"
+  ];
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  document.getElementById("message").textContent = messages[randomIndex];
+}
